ac0795033ddf10280a5cd07306ed6158cc0902d5
Arthur <arthurgustavobr@gmail.com>
2024-06-13 22:26:50 -0300

    Alterando cor e fazendo o botao adicionar funcionar

--- js/javascrip.js
+++ js/javascrip.js
@@ -1,8 +1,22 @@
+let amigos = [];
+
 const txtAmigo = document.querySelector('.sorteio-config');
 const btnAdicionar = document.querySelector('.add');
-const listaParticipantes = document.querySelector('amigos-add');
+const listaParticipantes = document.querySelector('.amigos-add');
+const mensagemErro = document.querySelector('.mensagem-erro')
 
 btnAdicionar.addEventListener('click', () => {
-    listaParticipantes.textContent += txtAmigo.value + ", ";
-    txtAmigo.value = '';
+
+    if (txtAmigo.value.length === 0) {
+        mensagemErro.textContent = "Imbecil Coloca o nome de alguém"
+    } else {
+        mensagemErro.textContent = ''
+        if (listaParticipantes.innerHTML === "") {
+            listaParticipantes.textContent = txtAmigo.value;
+        } else {
+            listaParticipantes.textContent += " , " + txtAmigo.value;
+        }
+        amigos.push(txtAmigo.value);
+        txtAmigo.value = '';
+    }
 })
