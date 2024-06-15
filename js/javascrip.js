9fd22e46de095a1973cb65b2c36bb9bfc206a4c1
Arthur <arthurgustavobr@gmail.com>
2024-06-14 22:23:40 -0300

    Adicionando Funções

--- js/javascrip.js
+++ js/javascrip.js
@@ -1,22 +1,42 @@
 let amigos = [];
 
-const txtAmigo = document.querySelector('.sorteio-config');
-const btnAdicionar = document.querySelector('.add');
-const listaParticipantes = document.querySelector('.amigos-add');
-const mensagemErro = document.querySelector('.mensagem-erro')
+const txtAmigo = document.querySelector(".sorteio-config");
+const btnAdicionar = document.querySelector(".add");
+const listaParticipantes = document.querySelector(".amigos-add");
+const mensagemErro = document.querySelector(".mensagem-erro");
 
-btnAdicionar.addEventListener('click', () => {
+btnAdicionar.addEventListener("click", () => {
+  if (ValidarAmigo()) {
+    adicionar();
+  }
+});
+txtAmigo.addEventListener("click", () => {
+  txtAmigo.value = "";
+});
 
-    if (txtAmigo.value.length === 0) {
-        mensagemErro.textContent = "Imbecil Coloca o nome de alguém"
-    } else {
-        mensagemErro.textContent = ''
-        if (listaParticipantes.innerHTML === "") {
-            listaParticipantes.textContent = txtAmigo.value;
-        } else {
-            listaParticipantes.textContent += " , " + txtAmigo.value;
-        }
-        amigos.push(txtAmigo.value);
-        txtAmigo.value = '';
-    }
-})
+function adicionar() {
+  if (amigos.includes(txtAmigo.value)) {
+    MostrarErro(`${txtAmigo.value} Ja Esta na lista adicione outro nome`);
+    return;
+  }
+  LimparMsgErro();
+  amigos.push(txtAmigo.value);
+  listaParticipantes.textContent = amigos.join(", ");
+  txtAmigo.value = "";
+}
+
+function ValidarAmigo() {
+  if (txtAmigo.value.length <= 3) {
+    MostrarErro('Insira um nome Valido')
+    return false;
+  }
+  return true;
+}
+
+function LimparMsgErro() {
+  mensagemErro.textContent = "";
+}
+
+function MostrarErro(textoMensagem) {
+    mensagemErro.textContent = `ERRO:${textoMensagem}`
+}
