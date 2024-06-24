1986de932c8f681eb922019ee59918ae4c41f3dc
ArthurTiengo <arthurgustavobr@gmail.com>
2024-06-24 19:19:40 -0300

    Fim

--- js/javascrip.js
+++ js/javascrip.js
@@ -2,8 +2,11 @@ let amigos = [];
 
 const txtAmigo = document.querySelector(".sorteio-config");
 const btnAdicionar = document.querySelector(".add");
+const btnSortear = document.querySelector(".sorteio")
+const btnReiniciar = document.querySelector(".reiniciar")
 const listaParticipantes = document.querySelector(".amigos-add");
 const mensagemErro = document.querySelector(".mensagem-erro");
+const sorteio = document.querySelector(".results")
 
 btnAdicionar.addEventListener("click", () => {
   if (ValidarAmigo()) {
@@ -13,6 +16,18 @@ btnAdicionar.addEventListener("click", () => {
 txtAmigo.addEventListener("click", () => {
   txtAmigo.value = "";
 });
+
+btnReiniciar.addEventListener('click', () => {
+  amigos = [];
+  listaParticipantes.textContent = '';
+  mensagemErro.textContent = '';
+  txtAmigo.value = '';
+
+});
+
+btnSortear.addEventListener('click', () => {
+  sortear()
+})
 
 function adicionar() {
   if (amigos.includes(txtAmigo.value)) {
@@ -33,10 +48,45 @@ function ValidarAmigo() {
   return true;
 }
 
+function reiniciar() {
+  txtAmigo.value = '';
+  amigos = []
+  listaParticipantes.innerHTML = ''
+  sorteio.innerHTML = "";
+}
+
 function LimparMsgErro() {
   mensagemErro.textContent = "";
 }
 
 function MostrarErro(textoMensagem) {
-    mensagemErro.textContent = `ERRO:${textoMensagem}`
+  mensagemErro.textContent = `ERRO:${textoMensagem}`
 }
+
+
+
+function embaralhar(lista) {
+  for (let indice = lista.length; indice; indice--) {
+    const indiceAleatorio = Math.floor(Math.random() * indice);
+    [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+  }
+}
+
+function sortear() {
+  if (amigos.length < 4) {
+    alert('Adicione ao menos 4 amigos!');
+    return;
+  }
+  embaralhar(amigos);
+  sorteio.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    if (i == amigos.length - 1) {
+      sorteio.innerHTML = sorteio.innerHTML + '<li>' + amigos[i] + '➜' + amigos[0] + '</li>'
+    } else {
+      sorteio.innerHTML = sorteio.innerHTML + '<li>' + amigos[i] + '➜' + amigos[i + 1] + '</li>'
+    }
+  }
+
+}
+
+
